e326f17d6e8f2c031b2dcf2c422f55a0e9887291
xmq291 <xmq291@gmail.com>
2018-08-05 12:11:00 -0400

    Part 4.1
    Part 4.1 of the project.

---
+++ Part 4_NodeJS/Coursera Server-Side/rest-server-passport/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    
+    /* Built on 11th Oct. 2017 via https://developers.facebook.com/apps/ 
+    with 491965291@qq.com account, 
+    client id: 102154857203951, 
+    client secret: 598900a191638aae77b0ddefde264c6a, 
+    name: conFusion,
+    contact email: xmq291@gmail.com
+    */
+    'facebook': { 
+        clientID: '102154857203951',
+        clientSecret: '598900a191638aae77b0ddefde264c6a',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
+    
+}
